3a969630bd1727f4b4c63785cc5d08ddce5bb3a9
wuhonglie <2429986613@qq.com>
2020-11-25 11:25:49 +0800

    完成代码测试

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module goblog
 go 1.15
 
 require (
-	github.com/go-sql-driver/mysql v1.5.0 // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/gorilla/mux v1.8.0
+	github.com/stretchr/testify v1.6.1
 )
--- tests/pages_test.go
+++ tests/pages_test.go
@@ -3,6 +3,7 @@ package tests
 import (
     "github.com/stretchr/testify/assert"
     "net/http"
+    "strconv"
     "testing"
 )
 
@@ -15,4 +16,51 @@ func TestHomePage(t *testing.T) {
     resp, err = http.Get(baseURL + "/")
     assert.NoError(t, err, "有错误发生， err 不为空")
     assert.Equal(t, 200, resp.StatusCode, "应返回状态码200")
+}
+
+func TestAboutPage(t *testing.T) {
+    baseURL := "http://localhost:3000"
+    var (
+        resp *http.Response
+        err error
+    )
+    resp, err = http.Get(baseURL + "/about")
+    assert.NoError(t, err, "有错误发生，err 不为空")
+    assert.Equal(t, 200, resp.StatusCode, "应该返回状态码200")
+}
+func TestAllPages(t *testing.T) {
+    baseURL := "http://localhost:3000"
+    var tests = []struct {
+        method string
+        url string
+        expected int
+    }{
+        {"GET", "/", 200},
+        {"GET", "/about", 200},
+        {"GET", "/notfound", 404},
+        {"GET", "/articles", 200},
+        {"GET", "/articles/create", 200},
+        {"GET", "/articles/3", 200},
+        {"GET", "/articles/3/edit", 200},
+        {"POST", "/articles/3", 200},
+        {"POST", "/articles", 200},
+        {"POST", "/articles/1/delete", 404},
+    }
+
+    for _, test := range tests {
+        t.Logf("当前请求 URL：%v \n", test.url)
+        var (
+            resp *http.Response
+            err error
+        )
+        switch {
+        case test.method == "POST":
+            data := make(map[string][]string)
+            resp,err = http.PostForm(baseURL+test.url,data)
+        default:
+            resp, err = http.Get(baseURL + test.url)
+        }
+        assert.NoError(t, err, "请求 "+ test.url+" 时报错")
+        assert.Equal(t, test.expected, resp.StatusCode, test.url+" 应返回状态码 "+strconv.Itoa(test.expected))
+    }
 }
