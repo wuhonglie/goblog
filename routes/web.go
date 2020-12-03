dc2698886fad7764ec4dc0f6b17045237bbc2c4b
wuhonglie <2429986613@qq.com>
2020-12-03 16:15:14 +0800

    美化文章列表页

--- routes/web.go
+++ routes/web.go
@@ -3,13 +3,16 @@ package routes
 import (
     "github.com/gorilla/mux"
     "goblog/app/http/controllers"
-    "goblog/app/http/middlewares"
     "net/http"
 )
 
 func RegisterWebRoutes(r *mux.Router) {
     pc := new(controllers.PagesController)
-    r.Use(middlewares.ForceHTML)
+    // 静态资源
+    r.PathPrefix("/css/").Handler(http.FileServer(http.Dir("./public")))
+    r.PathPrefix("/js/").Handler(http.FileServer(http.Dir("./public")))
+    // 中间件
+    //r.Use(middlewares.ForceHTML)
     r.HandleFunc("/", pc.Home).Methods("GET").Name("home")
     r.HandleFunc("/about", pc.About).Methods("GET").Name("about")
     r.NotFoundHandler = http.HandlerFunc(pc.NotFound)
