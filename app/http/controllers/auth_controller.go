235a271bc3c04b43916bf552f63985e219a18ba5
wuhonglie <2429986613@qq.com>
2020-12-23 18:47:41 +0800

    Flash 消息提示

--- app/http/controllers/auth_controller.go
+++ app/http/controllers/auth_controller.go
@@ -5,6 +5,7 @@ import (
     "goblog/app/models/user"
     "goblog/app/requests"
     "goblog/pkg/auth"
+    "goblog/pkg/flash"
     "goblog/pkg/view"
     "net/http"
 )
@@ -38,11 +39,12 @@ func (*AuthController) DoRegister(w http.ResponseWriter, r *http.Request){
         _user.Create()
         if _user.ID >0 {
             // 登录用户并跳转到首页
+            flash.Success("恭喜您注册成功")
             auth.Login(_user)
-           http.Redirect(w,r,"/",http.StatusFound)
+            http.Redirect(w,r,"/",http.StatusFound)
         }else{
-           w.WriteHeader(http.StatusInternalServerError)
-           fmt.Fprint(w, "注册失败，请联系管理员")
+            w.WriteHeader(http.StatusInternalServerError)
+            fmt.Fprint(w, "注册失败，请联系管理员")
         }
     }
 }
@@ -59,6 +61,7 @@ func (*AuthController) DoLogin(w http.ResponseWriter, r *http.Request) {
     // 2. 尝试登录
     if err := auth.Attempt(email,password); err == nil {
         // 登录成功
+        flash.Success("欢迎回来！")
         http.Redirect(w,r,"/",http.StatusFound)
     } else {
         // 3.失败，显示错误提示
@@ -72,5 +75,6 @@ func (*AuthController) DoLogin(w http.ResponseWriter, r *http.Request) {
 
 func (*AuthController) Logout(w http.ResponseWriter, r *http.Request) {
     auth.Logout()
+    flash.Success("您已退出登录")
     http.Redirect(w, r, "/", http.StatusFound)
 }
